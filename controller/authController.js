4ea1955cd0ce9bd019d9e521e5105612f12c76d7
Shruti Tiwari <sh03shruti@gmail.com>
2022-10-13 11:36:35 +0530

    login data ejs

--- controller/authController.js
+++ controller/authController.js
@@ -52,3 +52,22 @@ exports.signUp = catchAsync(async (req, res, next) => {
   });
   creatAndSendToken(newUser, 201, res);
 });
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //1) if email and password exist?
+  if (!email || !password) {
+    return next(new AppError("Please provide email and password", 400));
+  }
+
+  //2) check if user exist && password is correct
+
+  const user = await User.findOne({ email }).select("+password");
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError("Incorrect email or password", 401));
+  }
+
+  //3) if everything ok, send token to client.
+  creatAndSendToken(user, 200, res);
+});
