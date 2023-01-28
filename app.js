aacd446acc6fa7ccab643a96dc6e47571685ee81
Shruti Tiwari <sh03shruti@gmail.com>
2023-01-28 22:53:00 +0530

    update

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-/* eslint-disable no-console */
-const express = require("express");
-const morgan = require("morgan");
-const userRouter = require("./Router/userRoute");
-const path = require("path");
+// /* eslint-disable no-console */
+// const express = require("express");
+// const morgan = require("morgan");
+// const userRouter = require("./Router/userRoute");
+// const path = require("path");
 
-const app = express();
+// const app = express();
 
-//Development logging
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
-//serving static files
-app.use(express.static(`${__dirname}/public`));
+// //Development logging
+// if (process.env.NODE_ENV === "development") {
+//   app.use(morgan("dev"));
+// }
+// //serving static files
+// app.use(express.static(`${__dirname}/public`));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.set("views", path.join(__dirname, "./views"));
-app.set("view engine", "pug");
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.set("views", path.join(__dirname, "./views"));
+// app.set("view engine", "pug");
 
-//test middleware
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+// //test middleware
+// app.use((req, res, next) => {
+//   req.requestTime = new Date().toISOString();
+//   next();
+// });
 
-app.use("/api/v1/users", userRouter);
 // app.use("/api/v1/users", userRouter);
+// // app.use("/api/v1/users", userRouter);
 
-app.all("*", (req, res, next) => {
-  console.log("route not defined yet!", res.url);
-  // next(new AppError(`Cant find ${req.originalUrl} on this server!`, 404));
-});
+// app.all("*", (req, res, next) => {
+//   console.log("route not defined yet!", res.url);
+//   // next(new AppError(`Cant find ${req.originalUrl} on this server!`, 404));
+// });
 
-module.exports = app;
+// module.exports = app;
--- utils/catchAsync.js
+++
@@ -1,6 +0,0 @@
-module.exports = (fn) => {
-  return (req, res, next) => {
-    fn(req, res, next).catch(next);
-    //very imp.. if error occurs in the aysnc func then this catch will catch the error and call the global error handling middleware.
-  };
-};
